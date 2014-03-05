32bfdf16c1592db08d3869de49b04b7dde045aad
Harris Lloyd-levy <harris.lloydlevy@gmail.com>
2014-03-05 23:50:36 +1100

    Add callbacks to trigger messages on making/cancelling minyan

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -3,4 +3,27 @@ class Rsvp < ActiveRecord::Base
   belongs_to :yid
 
   validates_uniqueness_of :event_id, :scope => :yid_id
+  validates_uniqueness_of :yid_id, :scope => :event_id
+
+  after_create :check_minyan
+  before_destroy :check_cancel
+  
+private
+  def check_minyan
+    # This is called after an RSVP has been added. If we have 10 now send
+    # out message.
+    if event.num_rsvps == 10
+      logger.debug "Sending success message for event %d" % event.id
+      event.success_messages
+    end
+  end
+
+  def check_cancel
+    # This is called after an RSVP has been removed. If we have 9 now that
+    # the 10th just cancelled, so send out a message.
+    if event.num_rsvps == 10
+      logger.debug "Sending cancellation message for event %d" % event.id
+      event.cancel_messages
+    end
+  end
 end
