49f90ffbe784c492e4e723ba1b4a5078c2d932ba
Harris Lloyd-levy <harris.lloydlevy@gmail.com>
2014-03-09 01:46:02 +1100

    Change code to use 'ENV' to conform to Heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
-    APP_CONFIG['OAUTH_CLIENT_ID'],
-    APP_CONFIG['OAUTH_CLIENT_SECRET'],
+    ENV['OAUTH_CLIENT_ID'],
+    ENV['OAUTH_CLIENT_SECRET'],
     { name: 'google_login',
       scope: 'userinfo.email,userinfo.profile',
       approval_prompt: '' }
