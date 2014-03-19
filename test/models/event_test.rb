aadd670290f41b0de36332b43f06c827b41e35dc
Harris Lloyd-levy <harris.lloydlevy@gmail.com>
2014-03-19 22:25:13 +1100

    Add mailing/messaging tests

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -4,4 +4,24 @@ class EventTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  #
+  test "send reminders all" do
+    # Make sure the daily function sends out confirmation mails and
+    # reminders as appropriate
+    Event.send_event_reminders
+
+    ap ActionMailer::Base.deliveries
+  end
+
+  test "avoid reminders for regulars" do
+    # Make sure regulars who have RSVPd don't get reminders
+  end
+
+  test "confirmation to regulars" do
+    # Make sure regulars get reminders
+  end
+
+  test "confirmation to attendees" do
+    # Make sure attendees who are not regulats get the confirmation mail
+  end
 end
