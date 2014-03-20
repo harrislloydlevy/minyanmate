fa0925d6ba6a3d0e3b1ec5354b15dc00fe58d0d3
Harris Lloyd-levy <harris.lloydlevy@gmail.com>
2014-03-21 00:24:01 +1100

    Merge similiar change from main

--- test/controllers/yids_controller_test.rb
+++ test/controllers/yids_controller_test.rb
@@ -53,25 +53,15 @@ class YidsControllerTest < ActionController::TestCase
   end
 
   test "fail update yid" do
-<<<<<<< HEAD
     bad_email="12345"
-=======
-    bad_email = "12345"
->>>>>>> 87249a1d9e1e0642271b1860c5d308fcccb2a57a
 
     @yid = yids(:omni_login_yid)
     # Fake login as the user first.
     fake_login(@yid)
     post :update, id: @yid, yid: { email: bad_email, name: @yid.name, phone: @yid.phone }
-<<<<<<< HEAD
+    # Will return success if update failed and we are back at same page. Success redirects
     assert_response :success
     assert_not_nil flash[:error]
-=======
-
-    # Will return success if update failed and we are back at same page. Success redirects
-    # back to the view yid page.
-    assert_response(:success)
->>>>>>> 87249a1d9e1e0642271b1860c5d308fcccb2a57a
   end
 
   test "should get index" do
