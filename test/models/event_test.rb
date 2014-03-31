fbd90b0723ba8f37ab44c87d3ff832778eb28771
Harris Lloyd-Levy <harris.lloydlevy@gmail.com>
2014-03-31 15:57:36 +1100

    Time based testing. Only use delorean time setting where needed

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,6 +1,31 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
+  setup do
+    # Set the date to 1/1/2014 using delorean
+    Delorean.time_travel_to Date.new(2014,1,1)
+    ActionMailer::Base.deliveries = []
+  end
+
+  teardown do
+    Delorean.back_to_the_present
+  end
+
+
+  test "success message" do
+    # We setup a minyan with 9 people, now we add a 10th and make
+    # sure a success message is sent.
+    event = Event.find(events(:city_2))
+    event.attend(yids(:Oilom))
+
+    assert(
+      1,
+      ActionMailer::Base.deliveries
+        .select { |m| m.subject.include? "Minyan for CBD" }
+        .count
+    )
+  end
+
   # test "the truth" do
   #   assert true
   # end
@@ -10,10 +35,38 @@ class EventTest < ActiveSupport::TestCase
     # reminders as appropriate
     Event.send_event_reminders
 
-    ap ActionMailer::Base.deliveries
+    # Make sure we have a single confirmation for CBD Minyan
+    assert_equal(
+      1,
+      ActionMailer::Base.deliveries
+        .select { |m| m.subject.include? "Confirmation of CBD" }
+        .count
+    )
+
+    # Make sure we have single reminder for the next days CBD Minyan
+    assert_equal(
+      1,
+      ActionMailer::Base.deliveries
+        .select { |m| m.subject.include? "Reminder of CBD" }
+        .count
+    )
+
+    # Make sure we have no reminder for the erven shabbos minyan
+    # as all regulars have RSVP'd
+    assert_equal(
+      0,
+      ActionMailer::Base.deliveries
+        .select { |m| m.subject.include? "Reminder of Erev" }
+        .count
+    )
   end
 
-  test "avoid reminders for regulars" do
+  test "send reminder to regulars" do
+  end
+
+  test "avoid reminders for rsvpd regulars" do
+    # Get an event of 
+    @event = events(:city_1)
     # Make sure regulars who have RSVPd don't get reminders
   end
 
